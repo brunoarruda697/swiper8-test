8d1825e2a7454f6fc0336b13a0e0fc4b2ce6d01f
Bruno Costa <bruno.costa@privacy.com.br>
2022-06-14 14:31:50 -0300

    test swiper v8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import SwiperCore, { Navigation, Pagination, SwiperOptions, Zoom } from 'swiper';
+import { SwiperComponent } from "swiper/angular";
+
+SwiperCore.use([Zoom, Navigation, Pagination]);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
-  title = 'carousel-testt';
+  config: SwiperOptions = {
+    zoom: true,
+    slidesPerView: 1,
+    navigation: true,
+    centeredSlides: true,
+    direction: 'horizontal',
+    enabled: true,
+    pagination: true
+  };
+
+  onSwiper() {
+    console.log('swiper');
+  }
+  onSlideChange() {
+    console.log('slide change');
+  }
 }
